b9c2a2d0f15b835646a4206315461e6cc82439a8
rfid <rfid@mail.com>
2016-04-09 11:27:00 +0000

    soporte para led

--- src/main.py
+++ src/main.py
@@ -4,22 +4,24 @@ from MFRC522.MFRC522 import MFRC522
 import signal
 import time
 
-continue_reading = True
-MIFAREReader = MFRC522()
-
-cardA = [5,74,28,185,234]
-cardB = [83,164,247,164,164]
-cardC = [20,38,121,207,132]
+import RPi.GPIO as GPIO
 
 def end_read(signal, frame):
   global continue_reading
   continue_reading = False
   print "Ctrl+C captured, ending read."
-  MIFAREReader.GPIO_CLEAN()
-
+  #MIFAREReader.GPIO_CLEAN()
+  GPIO.cleanup()
 
 def main():
+
+  GPIO.setwarnings(False) 
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(11, GPIO.OUT)
+  GPIO.output(11, False) 
   signal.signal(signal.SIGINT, end_read)
+  continue_reading = True
+  MIFAREReader = MFRC522()
 
   while continue_reading:
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
@@ -29,8 +31,11 @@ def main():
     (status,backData) = MIFAREReader.MFRC522_Anticoll()
     if status == MIFAREReader.MI_OK:
       print "Card read UID: "+str(backData[0])+","+str(backData[1])+","+str(backData[2])+","+str(backData[3])+","+str(backData[4])
-    #APAGAR LEED
+      GPIO.output(11, True)
+      print "encender led"
+      #APAGAR LEED
     time.sleep(1)
+    GPIO.output(11, False)
 
 if __name__ == '__main__':
   main()
