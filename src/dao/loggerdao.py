2591020a7440c143d0b0ad5f79f5fa917b5bfae2
eloylp <eloy@sandboxwebs.com>
2016-04-09 14:31:15 +0200

    refacto and added card dao

--- src/dao/loggerdao.py
+++ src/dao/loggerdao.py
@@ -3,13 +3,14 @@ from src.datasource.mysql import MysqlSource
 
 class LoggerDAO(object):
     def __init__(self):
-        self.db = MysqlSource()
+        self.db = MysqlSource().get_db()
+        self.table_name = 'logger'
 
     def log_entry(self, event):
-        with self.db.get_cursor() as c:
-            c.execute("INSERT INTO logger SET event=%s", [event])
+        with self.db.cursor() as c:
+            c.execute("INSERT INTO "+self.table_name+" SET event=%s", [event])
 
     def get_entries(self):
-        with self.db.get_cursor() as c:
-            c.execute("SELECT * FROM logger order by moment DESC")
+        with self.db.cursor() as c:
+            c.execute("SELECT * FROM "+self.table_name+" order by moment DESC")
             return c.fetchall()
